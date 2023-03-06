d76f78d0ad0153d6feb2f8f45c861f07610fbccd
Fouzi92 <fouzi457@hotmail.fr>
2023-03-06 20:16:33 +0100

    Full-Stack-JS-1

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -10,6 +10,20 @@ router.post("/", (req,res) => {
     res.json({ message: req.body.message });
 })
 
+router.put('/:id', (req,res) => {
+    res.json({messageId: req.params.id })
+})
 
+router.delete("/:id" , (req, res) => {
+    res.json({ message: "Post supprimé id  : " +  req.params.id})
+})
+
+router.patch("/like-post/:id", (req,res) => {
+    res.json({ message: "Post liké : id :" + req.params.id})
+})
+
+router.patch("/dislike-post/:id", (req,res) => {
+    res.json({ message: "Post disliké : id :" + req.params.id})
+})
 
 module.exports = router
