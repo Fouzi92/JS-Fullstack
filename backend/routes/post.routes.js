9f1b00359b1b46f52b6131092ca8ed7ef2af6df6
Fouzi92 <fouzi457@hotmail.fr>
2023-03-08 19:00:20 +0100

    Full-Stack-JS-1

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,25 +1,14 @@
 const express = require("express");
-const { setPosts, getPosts } = require("../controllers/post.controller");
+const { setPosts, getPosts, editPost, deletePost, likePost, dislikePost } = require("../controllers/post.controller");
 const router = express.Router();
 
 router.get("/", getPosts);
+router.post("/", setPosts);
+router.put("/:id", editPost);
+router.delete("/:id", deletePost);
 
-router.post("/",setPosts )
+router.patch("/like-post/:id",likePost)
 
-router.put('/:id', (req,res) => {
-    res.json({messageId: req.params.id })
-})
-
-router.delete("/:id" , (req, res) => {
-    res.json({ message: "Post supprimé id  : " +  req.params.id})
-})
-
-router.patch("/like-post/:id", (req,res) => {
-    res.json({ message: "Post liké : id :" + req.params.id})
-})
-
-router.patch("/dislike-post/:id", (req,res) => {
-    res.json({ message: "Post disliké : id :" + req.params.id})
-})
+router.patch("/dislike-post/:id", dislikePost)
 
 module.exports = router
