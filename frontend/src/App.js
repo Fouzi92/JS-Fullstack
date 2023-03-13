ee687554c437522511eb6ff73d95d42b36fdd4ef
Fouzi92 <fouzi457@hotmail.fr>
2023-03-13 18:53:59 +0100

    Full-Stack-JS-1

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [userId, setUserId] = useState("")
+  return (
+    <div className="app-container">
+      <div className="login">
+        <h3>Bonjour</h3>
+        <input type="text"placeholder='Pseudo' onChange={ e=> setUserId(e.target.value)} />
+      </div>
+      {userId}
+    </div>
+  );
+};
+
+export default App;
